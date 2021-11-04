8c7c68a18ff82f230ffcc53d3a66d49aa6834fac
alex <arodriguez202020@gmail.com>
2021-11-03 19:39:09 -0500

    install and require libraries, create get request to signup page,  create post request to mailchimp api and store api key in environment variable file

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const apikey = process.env.API_KEY;
+  const jsonData = JSON.stringify(data);
+  const url = "https://us1.api.mailchimp.com/3.0/lists/ba234ba8fe";
+  const options = {
+    method: "POST",
+    auth: `alex1:${apikey}`,
+  };
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
